2ab5171570eb1a98a401b69d6cdd60d0a231e621
DATBOI <alexsimons9999@gmail.com>
2020-02-08 14:13:37 -0600

    More able to build checksums

--- buildSrc/AssetSync.ts
+++ buildSrc/AssetSync.ts
@@ -1,10 +1,10 @@
 import fs from 'fs';
 import path from 'path';
+import crypto from 'crypto';
 
 const assetDirectories = [
     'backgrounds',
     'stickers',
-    'themes'
 ]
 
 async function walkDir(dir: string): Promise<string[]> {
@@ -22,11 +22,48 @@ async function walkDir(dir: string): Promise<string[]> {
     return scannedDirectories.reduce((accum, files) => accum.concat(files), []);
 }
 
+function createChecksum(data: Buffer): string {
+    return crypto.createHash('md5')
+        .update(data)
+        .digest('hex');
+}
+
+interface StringDictionary<T> {
+    [key: string]: T
+}
 Promise.all(
     assetDirectories.map(directory =>
         walkDir(path.join(__dirname, '..', directory)))
 )
     .then(directories => directories.reduce((accum, dirs) => accum.concat(dirs), []))
-    .then(allAssets => {
-        console.log(allAssets);
+    .then(allAssets =>
+        Promise.all(
+            allAssets.map(assetPath =>
+                new Promise<Buffer>((res, rej) =>
+                    fs.readFile(assetPath, (err, dat) => {
+                        if (err) {
+                            rej(err)
+                        } else {
+                            res(dat);
+                        }
+                    }))
+                    .then(createChecksum)
+                    .then(checkSum => ({
+                        assetPath,
+                        checkSum
+                    }))
+            )
+        ).then((assetToCheckSums) =>
+            assetToCheckSums.reduce(
+                (accum: StringDictionary<string>, assetToChecksum) => {
+                    accum[assetToChecksum.assetPath] = assetToChecksum.checkSum;
+                    return accum;
+                }, {})
+        )
+    )
+    .then(assetToCheckSum => {
+        return assetToCheckSum;
+    })
+    .then(allNewAssets => {
+        console.log(allNewAssets);
     })
